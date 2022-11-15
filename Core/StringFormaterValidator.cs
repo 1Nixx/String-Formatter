2c1ec06e25f3c2b8ba27ed457ce3aceb9cc9a605
Nikita Hripach <nikita.hripach@gmail.com>
2022-11-15 23:37:58 +0300

    Implement string validator

--- Core/StringFormaterValidator.cs
+++ Core/StringFormaterValidator.cs
@@ -1,43 +1,120 @@
 ﻿using Core.Interfaces;
 
-
 namespace Core
 {
-	internal class StringFormaterValidator : IValidator<string>
+	public class StringFormaterValidator : IValidator<string>
 	{
 		public bool IsValid(string input)
 		{
-			int sum = 0;
+			int singleBraceCounter = 0;
+			int doubleBraceCounter = 0;
 
+			ParceState state = ParceState.Entry;
 			for (int i = 0; i < input.Length; i++)
 			{
-
-				if (sum > 2 || sum < 0 ||
-					(sum == 2 && input[i] == '}' && (i + 1 >= input.Length || input[i + 1] != '}')) ||
-					(sum == 1 && input[i] == '{' && input[i - 1] != '{'))
+				switch (state)
 				{
-					return false;
+					case ParceState.Entry:
+						if (input[i] == '{')
+							state = ParceState.LeftBrace;
+						else if (input[i] != '}')
+							state = ParceState.AnyText;
+						else
+							return false;
+						break;
+					case ParceState.LeftBrace:
+						singleBraceCounter++;
+
+						if (input[i] == '{')
+							state = ParceState.LeftDoubleBrace;
+						else if (input[i] == '}')
+							return false;
+						else if (IsValidIdentificatorChar(input[i]))
+							state = ParceState.Identificator;
+						else
+							return false;
+						break;
+					case ParceState.RightBrace:
+						singleBraceCounter--;
+
+						if (input[i] == '}')
+							state = ParceState.RightDoubleBrace;
+						else if (input[i] == '{')
+							state = ParceState.LeftBrace;
+						else 
+							state = ParceState.AnyText;
+						break;
+					case ParceState.LeftDoubleBrace:
+						singleBraceCounter--;
+						doubleBraceCounter++;
+
+						if (input[i] == '{')
+							state = ParceState.LeftBrace;
+						else if (input[i] == '}')
+							state = ParceState.RightBrace;
+						else
+							state = ParceState.AnyText;
+						break;
+					case ParceState.RightDoubleBrace:
+						singleBraceCounter++;
+						doubleBraceCounter--;
+
+						if (input[i] == '{')
+							state = ParceState.LeftBrace;
+						else if (input[i] == '}')
+							state = ParceState.RightBrace;
+						else
+							state = ParceState.AnyText;
+						break;
+					case ParceState.AnyText:
+						if (singleBraceCounter != 0)
+							return false;
+
+						if (input[i] == '{')
+							state = ParceState.LeftBrace;
+						else if (input[i] == '}')
+							state = ParceState.RightBrace;
+						else
+							state = ParceState.AnyText;
+						break;
+					case ParceState.Identificator:
+						if (singleBraceCounter != 1)
+							return false;
+
+						if (input[i] == '}')
+							state = ParceState.RightBrace;
+						else if (IsValidIdentificatorChar(input[i]))
+							state = ParceState.Identificator;
+						else
+							return false;
+						break;
+					default:
+						return false;
 				}
-
-				if (input[i] == '{')
-				{
-					sum++;
-				}
-
-				if (input[i] == '}')
-				{
-
-					sum--;
-				}
-
 			}
 
-			if (sum != 0)
-			{
-				return false;
-			}
+			if (state == ParceState.RightBrace)
+				singleBraceCounter--;
+			else if (state == ParceState.RightDoubleBrace)
+				singleBraceCounter++;
 
-			return true;
+			return singleBraceCounter == 0;
+		}
+
+		private bool IsValidIdentificatorChar(char item)
+		{
+			return "QWERTYUIOPASDFGHJKLZXCVBNM".Contains(char.ToUpper(item));
+		}
+
+		private enum ParceState
+		{
+			Entry,
+			LeftBrace,
+			RightBrace,
+			LeftDoubleBrace,
+			RightDoubleBrace,
+			AnyText,
+			Identificator
 		}
 	}
 }
